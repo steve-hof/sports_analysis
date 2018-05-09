c0dd779daffeae56fffc9f163f98242722144fe7
Steve Hof <steven.hof@icloud.com>
2018-05-09 15:18:57 -0700

    get and clean play by play data matching sports_vu data

--- NBA/README.md
+++ NBA/README.md
@@ -8,4 +8,6 @@ Most of this has been taken from [doingthedishes](http://www.eyalshafran.com/scr
 This is my playground for using BeautifulSoup and other files to scrape data from [basketball-reference.com](https://www.basketball-reference.com/). The player_info and player_detailed_info are from [doingthedishes](http://www.eyalshafran.com/scraping_basketball_reference.html), the rest are my own. (Using anything but the first table on a particular page of their website can be tricky).
 
 ## sports_vu_playground.py
-Sports Vu data used to be freely available to the public but was taken away a couple years ago. Thankfully [neilmj's github](https://github.com/neilmj/BasketballData) contains like 600 games worth. Since I'm fascinated by improving the efficiency of, and finding patterns in human movement with machine learning, I figured his dataset would be a great place to start learning. Note, this file is basically empty right now, haha 
+Sports Vu data used to be freely available to the public but was taken away a couple years ago. Thankfully [neilmj's github](https://github.com/neilmj/BasketballData) contains like 600 games worth. Since I'm fascinated by improving the efficiency of, and finding patterns in human movement with machine learning, I figured his dataset would be a great place to start learning.
+
+So far I've managed to turn the play by play data for a given game into something that looks kind of acceptable, but as of yet not sure it is useful
--- NBA/sports_vu_playground.py
+++ NBA/sports_vu_playground.py
@@ -1,25 +1,79 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import pandas as pd
+import numpy as np
 import matplotlib as mpl
-import matplotlib.animation as animation
 import json
+import requests
 
 
-# Animation function / loop
-def draw_court(ax):
-    img = mpimg.imread('scraped_data/nba_court_T.png')  # read image. I got this image from gmf05's github.
-    # didn't get very far today, haha
-    pass
+# need the play by play data so we know what sports_vu data we're looking at
+# this function
+
+def get_pbp_game_data(data):
+    header_data = {  # got the header from the py goldsberry library
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9' \
+                  ',image/webp,*/*;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive'
+    }
+    game_url = 'http://stats.nba.com/stats/playbyplayv2?EndPeriod=0&EndRange=0&GameID=' + data['gameid'] + \
+               '&RangeType=0&StartPeriod=0&StartRange=0'
+    page_request = requests.get(game_url, headers=header_data)
+    # get the header data and add it to json
+    headers = page_request.json()['resultSets'][0]['headers']
+
+    # get the actual data 'rowSet' is the key we need
+    pbp_game_data = page_request.json()['resultSets'][0]['rowSet']
+
+    # create data frame
+    df = pd.DataFrame(pbp_game_data, columns=headers)
+
+    # only want certain columns
+    cols = ['EVENTNUM', 'EVENTMSGTYPE', 'HOMEDESCRIPTION', 'VISITORDESCRIPTION',
+            'PLAYER1_TEAM_ABBREVIATION']
+    df = df[cols]
+
+    # names of columns are horrid
+    df['TEAM'] = df['PLAYER1_TEAM_ABBREVIATION']
+    df.drop('PLAYER1_TEAM_ABBREVIATION', inplace=True, axis=1)
+
+    return df
 
 
 def main():
     json_data = open('/Users/stevehof/school/comp/fun/Winning_in_Sports_with_Data/'
                      'NBA/scraped_data/sports_vu_2016.json')
+
+    # dictionary with three keys: ['gamedate', gameid', 'events']
     data = json.load(json_data)
+
+    # events have four keys: ['eventId', 'visitor', 'home', 'moments']
+    first_event = data['events'][0].keys()
+
+    # First moment of the first event (the numbers, in order are):
+    # 1) quarter number.
+    # 2) time of the event in milliseconds.
+    # 3) number of seconds left in the quarter
+    # 4) number of seconds left on the shot clock.
+    # No clue what 'none' represents, haha
+    first_moment = data['events'][0]['moments'][0]
+    play_by_play_df = get_pbp_game_data(data)
+
+    # make dictionary so I don't have to memorize event type codes
+    event_dict = {1: 'Make', 2: 'Miss', 3: 'Free Throw', 4: 'Rebound',
+                  5: 'OB or T/O or Steal', 6: 'Personal Foul', 7: 'Violation',
+                  8: 'Substitution', 9: 'Timeout', 10: 'Jump Ball',
+                  12: '?-start quarter 1', 13: '?- start quarter2'}
+
+
+    fill = 2
 
 
 if __name__ == '__main__':
